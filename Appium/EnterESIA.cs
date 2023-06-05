b894766c6710fb9b7274dc088078f8bafbc20a38
notamedia <notamedia@DESKTOP-6O2SB6O>
2023-06-05 23:33:14 +0300

    add platformversion

--- Appium/EnterESIA.cs
+++ Appium/EnterESIA.cs
@@ -11,6 +11,7 @@ using System.Net.Http.Json;
 using System.Net.Http.Headers;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
+using System;
 
 namespace AppiumTest
 {
@@ -19,30 +20,38 @@ namespace AppiumTest
     [TestFixture]
     public class EnterESIA
     {
+       
      
-       // private AndroidDriver<AndroidElement> _driver;
-        private RemoteWebDriver _driver;  
+        private AndroidDriver<AndroidElement> _driver;
+      
         string mainEsiaButtonXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.ScrollView/android.view.View[1]/android.view.View/android.widget.Button\r\n";
         string checkBoxPolicyXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.CheckBox[1]\r\n";
         string checkBoxMinCifraXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.CheckBox[2]\r\n";
         string enterEsiaButtonXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[1]/android.widget.Button";
         string EsiaFormXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.ViewGroup";
         string exitXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View\r\n";
-       // AndroidElement? mainEsiaButton;
-       // AndroidElement? checkBoxPolicy;
-       // AndroidElement? enterEsiaButton;
-       // AndroidElement? checkBoxMinCifra;
-       // AndroidElement? EsiaForm;
-       ///* AndroidElement?*/ Exit;
+        //AndroidElement? mainEsiaButton;
+        //AndroidElement? checkBoxPolicy;
+        //AndroidElement? enterEsiaButton;
+        //AndroidElement? checkBoxMinCifra;
+        //AndroidElement? EsiaForm;
+        //AndroidElement? Exit;
 
         [SetUp]
         public void Setup()
         {
+            //var driverOption = new DesiredCapabilities();
             var driverOption = new AppiumOptions();
-            driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, /*"4  WVGA (Nexus S)"*/"Pixel 2");
-           // _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
-            _driver = new RemoteWebDriver(new Uri("http://localhost:4724/wd/hub"), driverOption);
+            //driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            //driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, /*"4  WVGA (Nexus S)"*/"Pixel 2");
+            //_driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
+            //driverOption.AddAdditionalCapability("automationName", "UiAutomator1");
+            driverOption.AddAdditionalCapability("platformName", "Android");
+            driverOption.AddAdditionalCapability("platformVersion", "11.0");
+            driverOption.AddAdditionalCapability("deviceName", "Pixel 2");
+            //driverOption.AddAdditionalCapability("appPackage", "ru.msp.rf.app");
+            _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);    
+          
         }
 
         [Test]
@@ -51,12 +60,12 @@ namespace AppiumTest
             int case_id = 1132;
             //QaseApi.RunsInfo();
             Thread.Sleep(50);
-            var mainEsiaButton = _driver.FindElementByXPath(mainEsiaButtonXpath);
+           var mainEsiaButton = _driver.FindElementByXPath(mainEsiaButtonXpath);
             Thread.Sleep(50);
             mainEsiaButton.Click();
             Thread.Sleep(50);
-            var checkBoxPolicy = _driver.FindElementByXPath(checkBoxPolicyXpath);
-           var  checkBoxMinCifra = _driver.FindElementByXPath(checkBoxMinCifraXpath);
+           var checkBoxPolicy = _driver.FindElementByXPath(checkBoxPolicyXpath);
+           var checkBoxMinCifra = _driver.FindElementByXPath(checkBoxMinCifraXpath);
             var enterEsiaButton = _driver.FindElementByXPath(enterEsiaButtonXpath);
             checkBoxPolicy.Click();
             Thread.Sleep(50);
@@ -64,7 +73,7 @@ namespace AppiumTest
             Thread.Sleep(50);
             try
             {
-               var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
+             var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
             }
             catch
             {
@@ -73,11 +82,11 @@ namespace AppiumTest
                 enterEsiaButton.Click();
                 try
                 {
-                    var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
+                 var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
                 }
                 catch
                 {
-                   var Exit = _driver.FindElementByXPath(exitXpath);
+                    var Exit = _driver.FindElementByXPath(exitXpath);
                     Exit.Click();
                     //QaseApi.RunCaseResult(case_id, "passed");
                     //Assert.Pass();
