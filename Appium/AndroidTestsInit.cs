0cf531027d577faacc5cf5b85d2648ba66336517
notamedia <notamedia@DESKTOP-6O2SB6O>
2023-06-06 14:46:38 +0300

    refact

---
+++ Appium/AndroidTestsInit.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.iOS;
+
+namespace Appium
+{
+    public class AndroidTestsInit
+    {
+        protected AndroidDriver<AndroidElement> driver;
+        public AndroidTestsInit() { }
+
+        [SetUp]
+        public void Init()
+        {
+            AppiumOptions appiumOptions = new AppiumOptions();
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Pixel 2");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.0");
+            driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
+
--- Appium/EnterESIA.cs
+++ Appium/EnterESIA.cs
@@ -12,68 +12,46 @@ using System.Net.Http.Headers;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using System;
+using Appium;
 
 namespace AppiumTest
 {
-
-
     [TestFixture]
-    public class EnterESIA
+    public class EnterESIA: AndroidTestsInit
     {
-       
-     
-        private AndroidDriver<AndroidElement> _driver;
-      
+        public EnterESIA() : base() { } 
         string mainEsiaButtonXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.widget.ScrollView/android.view.View[1]/android.view.View/android.widget.Button\r\n";
         string checkBoxPolicyXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.CheckBox[1]\r\n";
         string checkBoxMinCifraXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.CheckBox[2]\r\n";
         string enterEsiaButtonXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ScrollView/android.view.View[1]/android.widget.Button";
         string EsiaFormXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.ViewGroup";
         string exitXpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View\r\n";
-        //AndroidElement? mainEsiaButton;
-        //AndroidElement? checkBoxPolicy;
-        //AndroidElement? enterEsiaButton;
-        //AndroidElement? checkBoxMinCifra;
-        //AndroidElement? EsiaForm;
-        //AndroidElement? Exit;
-
-        [SetUp]
-        public void Setup()
-        {
-            //var driverOption = new DesiredCapabilities();
-            var driverOption = new AppiumOptions();
-            //driverOption.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            //driverOption.AddAdditionalCapability(MobileCapabilityType.DeviceName, /*"4  WVGA (Nexus S)"*/"Pixel 2");
-            //_driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);
-            //driverOption.AddAdditionalCapability("automationName", "UiAutomator1");
-            driverOption.AddAdditionalCapability("platformName", "Android");
-            driverOption.AddAdditionalCapability("platformVersion", "11.0");
-            driverOption.AddAdditionalCapability("deviceName", "Pixel 2");
-            //driverOption.AddAdditionalCapability("appPackage", "ru.msp.rf.app");
-            _driver = new AndroidDriver<AndroidElement>(new Uri("http://localhost:4723/wd/hub"), driverOption);    
-          
-        }
-
+        AndroidElement? mainEsiaButton;
+        AndroidElement? checkBoxPolicy;
+        AndroidElement? enterEsiaButton;
+        AndroidElement? checkBoxMinCifra;
+        AndroidElement? EsiaForm;
+        AndroidElement? Exit;
         [Test]
         public void EntranceEsiaWithoutCheckbox()
         {
-            int case_id = 1132;
+             //int case_id = 1132;
             //QaseApi.RunsInfo();
             Thread.Sleep(50);
-           var mainEsiaButton = _driver.FindElementByXPath(mainEsiaButtonXpath);
+            mainEsiaButton = driver.FindElementByXPath(mainEsiaButtonXpath);
             Thread.Sleep(50);
             mainEsiaButton.Click();
             Thread.Sleep(50);
-           var checkBoxPolicy = _driver.FindElementByXPath(checkBoxPolicyXpath);
-           var checkBoxMinCifra = _driver.FindElementByXPath(checkBoxMinCifraXpath);
-            var enterEsiaButton = _driver.FindElementByXPath(enterEsiaButtonXpath);
+            checkBoxPolicy = driver.FindElementByXPath(checkBoxPolicyXpath);
+            checkBoxMinCifra = driver.FindElementByXPath(checkBoxMinCifraXpath);
+            enterEsiaButton =  driver.FindElementByXPath(enterEsiaButtonXpath);
             checkBoxPolicy.Click();
             Thread.Sleep(50);
             enterEsiaButton.Click();
             Thread.Sleep(50);
             try
             {
-             var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
+             var EsiaForm = driver.FindElementByXPath(EsiaFormXpath);
             }
             catch
             {
@@ -82,11 +60,11 @@ namespace AppiumTest
                 enterEsiaButton.Click();
                 try
                 {
-                 var EsiaForm = _driver.FindElementByXPath(EsiaFormXpath);
+                   EsiaForm = driver.FindElementByXPath(EsiaFormXpath);
                 }
                 catch
                 {
-                    var Exit = _driver.FindElementByXPath(exitXpath);
+                    Exit = driver.FindElementByXPath(exitXpath);
                     Exit.Click();
                     //QaseApi.RunCaseResult(case_id, "passed");
                     //Assert.Pass();
