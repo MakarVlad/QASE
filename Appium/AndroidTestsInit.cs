74a8bf9ab29282e94087b8f7d0a8b2494c5d909f
notamedia <notamedia@DESKTOP-6O2SB6O>
2023-06-06 20:18:00 +0300

    down ver emulator -10

--- Appium/AndroidTestsInit.cs
+++ Appium/AndroidTestsInit.cs
@@ -22,7 +22,7 @@ namespace Appium
             AppiumOptions appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Pixel 2");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.0");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
             driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
         }
 
