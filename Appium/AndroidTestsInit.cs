3deb415da5e33afaa213e35386c62a86ac039b8e
notamedia <notamedia@DESKTOP-6O2SB6O>
2023-06-07 11:05:04 +0300

    platform ver down 10

--- Appium/AndroidTestsInit.cs
+++ Appium/AndroidTestsInit.cs
@@ -25,7 +25,7 @@ namespace Appium
             AppiumOptions appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Pixel 2");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.0");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.App, "C:\\Users\\notamedia\\source\\repos\\Appium\\Appium\\MSP_v.1.2.0(344-release).apk");
             driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
 
