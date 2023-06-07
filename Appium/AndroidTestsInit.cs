21eae48b97660d6356cbe19799906a700f039f31
notamedia <notamedia@DESKTOP-6O2SB6O>
2023-06-07 15:47:59 +0300

    add Path.Combine

--- Appium/AndroidTestsInit.cs
+++ Appium/AndroidTestsInit.cs
@@ -23,11 +23,12 @@ namespace Appium
         [SetUp]
         public void Init()
         {
+            string appPath = Path.Combine(Environment.CurrentDirectory, "MSP_v.1.2.0(344-release).apk");
             AppiumOptions appiumOptions = new AppiumOptions();
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, "Pixel 2");
             appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
-            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "10.0");
-            appiumOptions.AddAdditionalCapability(MobileCapabilityType.App, "MSP_v.1.2.0(344-release).apk");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.PlatformVersion, "11.0");
+            appiumOptions.AddAdditionalCapability(MobileCapabilityType.App, appPath);
             driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), appiumOptions);
 
             touchAction = new TouchAction(driver);
